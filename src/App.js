79ec8b3be23d5aef711e91ad876fb236f0095f5d
Katie Dickson <katieedicksonn1@gmail.com>
2020-11-06 18:29:14 -0800

    everything is working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import API from "./utils/API";
 import Table from "./components/Table"
+import Header from "./components/Header"
 class App extends React.Component {
 
   state = {
@@ -13,11 +14,34 @@ class App extends React.Component {
         this.setState({ employees: res.data.results })
       })
   }
+  
+  handleSortByFirst = () => {
+    if (this.state) {
+        const sortFirst = this.state.employees.sort((a,b) => (a.name.first > b.name.first)?1 : -1);
+        this.setState({ filtered: sortFirst });
+    }
+};
+handleSortByLast = () => {
+  if (this.state) {
+      const sortLast = this.state.employees.sort((a,b) => (a.name.last > b.name.last)?1 : -1);
+      this.setState({ filtered: sortLast });
+  }
+};
+
   render() {
     return (
       <div className="App">
+      <Header />
+      <br />
+      <div className="btns">
+      <button onClick={this.handleSortByFirst}> this will filter by first name</button>
+      <button onClick={this.handleSortByLast}> this will filter by last name</button>
+      </div>
+      <br />
+
         {this.state.employees.map(employee => (
           <Table
+
             picture={employee.picture.medium}
             first={employee.name.first}
             last={employee.name.last}
--- src/components/Table.js
+++ src/components/Table.js
@@ -2,27 +2,52 @@ import React from "react";
 
 function Table(props) {
   return (
-    <div className="card">
-      <div className="img-container">
-        <img alt="img" src={props.picture} />
-      </div>
-      <div className="content">
-        <ul>
-          <li>
-            Name:{props.first} {props.last}
-          </li>
-          <li>
-            Age: {props.age}
-          </li>
-          <li>
-            E-mail: {props.email}
-          </li>
-          <li>
-            Phone: {props.phone}
-          </li>
-        </ul>
-      </div>
-    </div>
+      
+    // <div className="card">
+
+    //   <div className="img-container">
+    //   <div className="content row">
+    //     <div className= "col">
+    //     <img alt="img" src={props.picture} />
+    //     </div>
+    //   <div className="content col">
+    //     <ul>
+    //       <li>
+    //         Name:{props.first} {props.last}
+    //       </li>
+    //       <li>
+    //         Age: {props.age}
+    //       </li>
+    //       <li>
+    //         E-mail: {props.email}
+    //       </li>
+    //       <li>
+    //         Phone: {props.phone}
+    //       </li>
+    //     </ul>
+    //     </div>
+    //     </div>
+    //   </div>
+    // </div>
+
+
+
+
+<div className="card">
+<div className="content row">
+<img alt="img" src={props.picture} />
+  
+<div className="content">
+<p>Name:{props.first} {props.last}</p>
+<p>Age: {props.age}</p>
+<p>E-Mail: {props.email}</p>
+<p>Phone: {props.phone}</p>
+  </div>
+  </div>
+</div>
+
+
+
   );
 }
 
